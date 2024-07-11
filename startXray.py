b932817f26c1dc1525bce1afb843a777acc977ea
Ali Bijandi <73279127+alibij@users.noreply.github.com>
2024-07-11 09:35:48 +0330

    start service set for all os

--- startXray.py
+++ startXray.py
@@ -1,20 +1,32 @@
 import subprocess
+import platform
+import os
 
-
-def start_core (xray_exe_path = r'.\xray.exe'  ,config_file_path = r'.\config.json'):
-    command = [xray_exe_path, '-c', config_file_path]
+def start_core(xray_path='./xray', config_file_path='./config.json'):
+    # Adjust the executable name based on the OS
+    system = platform.system()
+    if system == 'Windows':
+        xray_path = xray_path + '.exe'
+        
+    command = [xray_path, '-c', config_file_path]
     try:
         process = subprocess.Popen(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         return {'pid': process.pid}
-
+        
     except Exception as e:
-        print(f"Error running xray.exe: {e}")
+        print(f"Error running {xray_path}: {e}")
 
 
 def stop_task (pid):
 
     try:
-        command = ['taskkill', '/F', '/PID', str(pid)]
+        system = platform.system()
+
+        if system == 'Windows':
+            command = ['taskkill', '/F', '/PID', str(pid)]
+        else:
+            command = ['kill', '-9', str(pid)]
+
         subprocess.run(command, capture_output=True, text=True, check=True)
         return True
     
