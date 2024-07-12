ba781e4585d84c1e1b975ce2bcd96bf74590138a
alibijandi <alibijandi75@gmail.com>
2024-07-12 21:17:36 +0330

    remove useless code and add main

--- speedtest.py
+++ speedtest.py
@@ -4,57 +4,9 @@ from aiohttp import ClientSession, ClientTimeout
 import time
 
 
-# async def __download_speed_test(url, proxy, download_timeout):
-#     speed = -1
-#     try:
-#         timeout = ClientTimeout(total=download_timeout)
-
-#         async with ClientSession(timeout=timeout) as session:
-#             async with session.get(url, proxy=proxy) as response:
-#                 if response.status == 200:
-
-#                     startTime = time.time()
-#                     data = 0
-#                     bufferSize = 1024 * 1024
-
-#                     while True:
-#                         d = await response.content.read(bufferSize)
-#                         if not d:
-#                             break
-#                         data += len(d)
-
-#                     endTime = time.time()
-#                     download_time = endTime - startTime
-#                     speed = (data / download_time) / (1024 * 1024)
-#                     print("url:",url,"speed:","{:.2f}".format(speed))
-#                 else:
-#                     print(
-#                         f"Failed to download from {url}. Status code: {response.status}")
-#     except Exception as e:
-#         # print(f"Exception occurred for {url} with proxy {proxy}: {e}")
-#         pass
-#     return speed           
-
-
-# async def __test_urls_with_proxies(urls: List[str], proxies: List[Optional[str]], download_timeout):
-#     speedTest = []
-
-#     for url in urls:
-#         for proxy in proxies:
-#             speedTest.append(await __download_speed_test(url, proxy, download_timeout))
-
-#     return speedTest
-
-
 async def speedTest(testUrl='https://speed.cloudflare.com/__down?bytes=1000000', proxy=None, timeout=30):
 
-    # speedTest = []
-
-    # for url in testUrl:
-    #     for proxy in proxies:
-    #         speedTest.append(await __download_speed_test(url, proxy, timeout))
-
-    speed = -1
+    speed = 0
     try:
         timeout = ClientTimeout(total=timeout)
 
@@ -83,5 +35,10 @@ async def speedTest(testUrl='https://speed.cloudflare.com/__down?bytes=1000000',
         pass
     return float("{:.2f}".format(speed))
 
-    return speedTest
+async def main():
+    speed = await speedTest()
+    print(f'Current Speed is : {speed} MB/s')
 
+if __name__ == "__main__":
+    asyncio.run(main())
+    
