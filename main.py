c8abe34b78fdeb9539dc745cf786d6e8674a67ea
alibijandi <alibijandi75@gmail.com>
2024-07-12 21:14:18 +0330

    add command for better UX

--- main.py
+++ main.py
@@ -22,14 +22,18 @@ async def main(test_limit, speedtelorance):
     print('Getting the latest servers')
     file = create_config(fetch_and_decode_data())
 
-    task = start_core()
+    task = start_core(config_file_path='./testconfig.json')
+
+    print('Getting the best server suitable for your internet')
 
     servers = await testPing(proxies=[f'http://localhost:{i}' for i in range(file['startPort'], file['lastPort']+1)])
 
     proxies = {}
     for i, ping in enumerate(servers):
         port = i+file['startPort']
-        proxies[port] = {'port': port, 'proxyUrl': f'http://localhost:{port}', 'connectionUrl': await removeRemark(file['serverList'][i], i), 'ping': ping, 'downloadSpeed': 0}
+        proxies[port] = {'port': port, 'proxyUrl': f'http://localhost:{port}',
+                         'connectionUrl': await removeRemark(file['serverList'][i], i),
+                         'ping': ping, 'downloadSpeed': 0}
 
     clashFile = ''
     serverfile = ''
@@ -37,38 +41,49 @@ async def main(test_limit, speedtelorance):
     for i in proxies:
         if proxies[i]['ping'] > 0:
             activeServer.append(proxies[i])
-    activeServer = sorted(activeServer, key=lambda x: x['ping'])
-    highestspeed = 0.00
-    speed = 0.00
-    for i, server in enumerate(activeServer):
-        highestspeed = float(speed) if float(
-            speed) > highestspeed else highestspeed
 
-        speed = await speedTest(proxy=server['proxyUrl'])
-        activeServer[i]['downloadSpeed'] = speed
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print(f'current server speed is {speed} MB/s')
-        print(f'highest server speed is {highestspeed} MB/s')
-        serverlen = len(activeServer) if test_limit < 0 else test_limit
-        print(f'\n   {i}/{serverlen} tested')
-        if i == test_limit:
+    activeServer = sorted(activeServer, key=lambda x: x['ping'])
+    highestserver = {}
+    speed = 0.00
+    netspeed = await speedTest()
+
+    for i, server in enumerate(activeServer):
+
+        if i == test_limit or speed >= (netspeed*speedtelorance):
+
+            connect_to_fastest(highestserver['connectionUrl'])
             break
+
+        activeServer[i]['downloadSpeed'] = await speedTest(proxy=server['proxyUrl'])
+        speed = activeServer[i]['downloadSpeed']
+
+        if 'downloadSpeed' not in highestserver:
+            highestserver = server
+        else:
+            highestserver = server if server['downloadSpeed'] > highestserver['downloadSpeed'] else highestserver
+
+        clear()
+        print(f'Current Speed is : {netspeed} MB/s')
+        print(f'highest server speed is {highestserver["downloadSpeed"]} MB/s')
+        print(f'current server speed is {speed} MB/s')
+
+        serverlen = test_limit if (test_limit > 0 and test_limit <= len(
+            activeServer)) else len(activeServer)
+
+        print_loading_bar(i+1, 0,serverlen)
+
     stop_task(task['pid'])
-    activeServer = sorted(activeServer, key=lambda x: float(
-        x['downloadSpeed']), reverse=True)
+    activeServer = sorted(
+        activeServer, key=lambda x: x['downloadSpeed'], reverse=True)
     for i in activeServer:
         clashFile += f'{i["connectionUrl"]}\n'
         serverfile += f'{i}\n'
 
     encoded_bytes = base64.b64encode(clashFile.encode('utf-8'))
     encoded_string = encoded_bytes.decode('utf-8')
-    with open("./serverlist.txt", 'w', encoding='utf-8') as f:
-        f.write(serverfile)
 
-    with open("./clashfile.txt", 'w', encoding='utf-8') as f:
-        f.write(encoded_string)
-
-
+    make_file(serverfile, "./serverlist.txt")
+    make_file(encoded_string, "./clashfile.txt")
 
 
 if __name__ == "__main__":
