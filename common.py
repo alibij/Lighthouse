c04b0d52358934f70de118c6727025865569df80
alibijandi <alibijandi75@gmail.com>
2024-07-12 21:10:22 +0330

    make common

---
+++ common.py
@@ -0,0 +1,43 @@
+import time
+import sys
+import os
+import base64
+import json
+
+
+async def removeRemark(url, num):
+
+    mode = url.split('://')
+    mode[0] = mode[0].lower()
+
+    if mode[0] in ['ss', 'trojan']:
+        return (f'{url.split("#")[0]}#Lighthouse.{num}')
+
+    elif mode[0] in ['vmess', 'vless']:
+
+        decoded_data = base64.b64decode(mode[1])
+        u = json.loads(decoded_data.decode('utf-8'))
+        u['ps'] = f'Lighthouse.{num}'
+        u = json.dumps(u, ensure_ascii=False)
+        eb = base64.b64encode(u.encode('utf-8'))
+        es = eb.decode('utf-8')
+        return (f'{mode[0]}://{es}')
+
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def make_file(data: any, file_name: str, mode='w', encoding='utf-8'):
+    with open(file_name, mode, encoding=encoding) as f:
+        f.write(data)
+
+
+
+def print_loading_bar(current, min_val, max_val, length=80):
+    progress = (current - min_val) / (max_val - min_val)
+    percent = int(progress * 100)
+    bar_length = int(length * progress)
+    bar = '[' + '|' * bar_length + ' ' * (length - bar_length) + '] ' + f'{percent} %'
+    sys.stdout.write('\r' + bar)
+    sys.stdout.flush()
